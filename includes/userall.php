57842cf05fbfd354b231b63f349aa4d19c14eac8
Misael Villaseñor <75335236+MisaelVillasenor@users.noreply.github.com>
2022-05-23 23:11:58 -0500

    Add files via upload

---
+++ includes/userall.php
@@ -0,0 +1,38 @@
+<?php
+
+include 'db.php'
+class User extends DB;{
+$private $nombre;
+private $username;
+public function user_Exists($user $pass){
+
+  $md5pass = md5($pass);  
+
+  $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE username = :user AND password = :pass');
+    $query-> execute(['user' => $user, 'pass' => $md5pass]);
+    
+    if($query->rowCount()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+public function setUser($user){
+    $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE correo = :user');
+    $query->execute(['user' => $user]);
+
+    foreach ($query as $currentUser) {
+        $this->nombre = $currentUser['nombre'];
+        $this->usename = $currentUser['username'];
+    }
+
+
+}
+
+public function getNombre(){
+    return $this->nombre;
+}
+}
+
+?>
